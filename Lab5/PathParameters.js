e78ffa6c68434e8cd26ff5d16f0ace4cb0c98b04
Quang Gia Nguyen <91767053+LordChunky@users.noreply.github.com>
2024-11-09 16:54:39 -0500

    Finished 3.2.3

---
+++ Lab5/PathParameters.js
@@ -0,0 +1,19 @@
+export default function PathParameters(app) {
+    // route expects 2 path parameters after /lab5/add retrieve path parameters as strings parse 
+    // as integers and adds sum as string sent back as response don't send integers since can be 
+    // interpreted as status
+    app.get("/lab5/add/:a/:b", (req, res) => {
+        const { a, b } = req.params;
+        const sum = parseInt(a) + parseInt(b);
+        res.send(sum.toString());
+    });
+
+    // route expects 2 path parameters after /lab5/subtract retrieve path parameters as strings
+    // parse as integers and subtracts subtraction as string sent back as response response is 
+    // converted to string otherwise browser would interpret integer response as a status code
+    app.get("/lab5/subtract/:a/:b", (req, res) => {
+        const { a, b } = req.params;
+        const sum = parseInt(a) - parseInt(b);
+        res.send(sum.toString());
+    });
+};
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from 'express';
-import Hello from "./Hello.js"
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
 
-const app = express()
+const app = express();
+app.use(express.json());
+Lab5(app);
 Hello(app)
 
-  
-app.listen(4000)
+// uses the remote PORT environment variable if available, or port 4000 when running locally
+app.listen(process.env.PORT || 4000)
